df5145ff44cbc0b83141a5b07df0fe744730c7bf
yyhh789 <129481978+yyhh789@users.noreply.github.com>
2023-06-30 13:00:12 +0900

    230630

--- project003/js/javascript.js
+++ project003/js/javascript.js
@@ -1,6 +1,36 @@
 $(document).ready(function(){
+    
+    let ht = $(window).height();
+    
+    $('section').height(ht);
+
+    $(window).resize(function(){
+        let ht = $(window).height();
+    
+        $('section').height(ht);
+    });
+
+            
 
 
+// section 위에서 마우스를 올릴떄,내릴떄 이벤트가 발생한다.
+    $('section').mousewheel(function(event,delta){
+        if(delta>0){
+            // 나의 이전요소의 높이값
+            let prev = $(this).prev().offset().top 
+
+
+
+            $('html,body').stop().animate({scrollTop:prev},800)
+        }
+        else if (delta<0){
+            let next = $(this).next().offset().top 
+
+
+
+            $('html,body').stop().animate({scrollTop:next},800)
+        }
+    })
 
     $('.sec1').mousemove(function(e){
         let x = e.pageX;
@@ -13,26 +43,4 @@ $(document).ready(function(){
 
     });
 
-
-
-    
-    $('section').mousewheel(function(event,delta){
-        if(delta>0){
-            
-            let prev = $(this).prev().offset().top 
-
-
-
-            $('html,body').stop().animate({scrollTop:prev},1400,'easeOutBounce')
-        }
-        else if (delta<0){
-            let next = $(this).next().offset().top 
-
-
-
-            $('html,body').stop().animate({scrollTop:next},1400,'easeOutBounce')
-        }
-    });
-
-
-})
+}) //제이쿼리 끝
